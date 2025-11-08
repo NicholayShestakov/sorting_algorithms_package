624839ae53cd7b9f0232dd6eb9a11a4b8dfdea24
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-11-08 16:08:31 +0300

    Update bubble sort tests

--- test/test_bubble_sort.py
+++ test/test_bubble_sort.py
@@ -5,18 +5,14 @@ from sorting_algorithms.bubble_sort import bubble_sort
 
 def test_easy():
     lst = [3, 1, 2]
-    bubble_sort(lst)
-    assert lst == [1, 2, 3]
+    assert bubble_sort(lst) == [1, 2, 3]
 
 
 def test_empty():
     lst = []
-    bubble_sort(lst)
-    assert lst == []
+    assert bubble_sort(lst) == []
 
 
 def test_random():
     lst = [randint(-1000, 1000) for _ in range(randint(1, 1000))]
-    lst_copy = lst.copy()
-    bubble_sort(lst)
-    assert lst == sorted(lst_copy)
+    assert bubble_sort(lst) == sorted(lst)
